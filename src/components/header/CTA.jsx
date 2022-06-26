f2113c0da5f77aabed3f05bd93babbc109f46ed4
stroke-prediction <108219244+stroke-prediction@users.noreply.github.com>
2022-06-26 22:33:48 +0600

    Add files via upload

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+      <div className="cta">
+          <a className='btn' href='../../assets/Resume of Nitish Biswas.pdf' download='Resume of Nitish Biswas.pdf'>Download CV</a>
+          <a className='btn btn-primary' href='#contact'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
